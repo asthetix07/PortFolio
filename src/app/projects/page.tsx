d73c9d90452dc22c9265eb7fffd17110206c4476
Akash Kumar <am7348654@gmail.com>
2026-06-26 16:36:56 +0530

    Dynamic page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,6 +2,8 @@ import { Column, Heading, Meta, Schema } from "@once-ui-system/core";
 import { baseURL, about, person, work } from "@/resources";
 import { Projects } from "@/components/work/Projects";
 
+export const dynamic = "force-dynamic";
+
 export async function generateMetadata() {
   return Meta.generate({
     title: "Projects – " + person.name,
